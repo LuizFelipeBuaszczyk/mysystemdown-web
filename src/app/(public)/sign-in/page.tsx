7e10333f12b8e53f4516f0bfd21e6df552c8f47d
LuizFelipeBuaszczyk <luizfelipebuaszczyk@hotmail.com>
2026-03-22 16:49:09 -0300

    feat: Tela de login
    Header possui navbar para navegação entre as páginas públicas

--- src/app/(public)/sign-in/page.tsx
+++ src/app/(public)/sign-in/page.tsx
@@ -2,19 +2,23 @@
 
 import { SyntheticEvent } from 'react';
 
+import styles from '@/app/(public)/sign-in/signin.module.css';
+
 export default function SignIn() {
 
     return (
-        <div>
+        <div className={styles.container}>
             <h1>Login</h1>
 
             <form onSubmit={() => {}}>
-                <label htmlFor="">Email</label>
-                <input type="email" name="email" id="emailInput" />
-
-                <label htmlFor="">Password</label>
-                <input type="password" name="password" id="passwordInput" />
-
+                <section className={styles.input}>
+                    <label htmlFor="">Email</label>
+                    <input type="email" name="email" id="emailInput" />
+                </section>
+                <section className={styles.input}>
+                    <label htmlFor="">Password</label>
+                    <input type="password" name="password" id="passwordInput" />
+                </section>                
                 <button type="submit">Login</button>
             </form>
         </div>
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,20 @@
+import styles from '@/components/header/header.module.css';
 
+import Link from 'next/link';
 
 export default function Header(){
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
+        <header className={styles.container}>
+            <Link href='/'> 
+                <h1>MySystemDown</h1>
+            </Link>
+            <nav>
+                <ul>
+                    <li className={styles.linkItem}>
+                        <Link href='/sign-in'>Sign-IN</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     );
 }
